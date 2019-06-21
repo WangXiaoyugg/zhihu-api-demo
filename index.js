83cb1430acb53be81fd0abcec93594cf05a44f3c
GarenWang <2270099149@qq.com>
2019-06-21 23:23:50 +0800

    feature: 第一个koa程序

---
+++ index.js
@@ -0,0 +1,10 @@
+const Koa = require("koa")
+const app = new Koa()
+
+app.use((ctx) => {
+    ctx.body = "Hello World"
+})
+
+app.listen(8000,() => {
+    console.log("server is start at localhost:8000")
+})
