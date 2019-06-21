fb92cf585bb831c432f1915dd15290f7aeee138b
GarenWang <2270099149@qq.com>
2019-06-22 00:01:54 +0800

    feature: 第一个中间件和洋葱模型

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Koa = require("koa")
 const app = new Koa()
 
-app.use((ctx) => {
+app.use((ctx, next) => {
+    console.log(1)
+    next()
     ctx.body = "Hello World!"
+    console.log(2)
+})
+
+app.use((ctx, next) => {
+    console.log(3);
+    next()
+    console.log(4)
+})
+app.use((ctx, next) => {
+    console.log(5);
 })
 
 app.listen(8000,() => {
