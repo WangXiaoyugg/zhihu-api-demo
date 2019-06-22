2529d9af5ff2102f769b8ceb88e6dccaa7f2f2d0
GarenWang <2270099149@qq.com>
2019-06-22 08:53:29 +0800

    feature: 第一个路由中间件

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const Koa = require("koa")
 const app = new Koa()
 
-app.use((ctx, next) => {
-    console.log(1)
-    next()
-    ctx.body = "Hello World!"
-    console.log(2)
-})
-
-app.use((ctx, next) => {
-    console.log(3);
-    next()
-    console.log(4)
-})
-app.use((ctx, next) => {
-    console.log(5);
+app.use((ctx) => {
+    if(ctx.url === '/') {
+        ctx.body = "这是首页"
+    } else if(ctx.url === '/users') {
+        if(ctx.method === "GET") {
+            ctx.body = "这是用户列表页"
+        } else if(ctx.method === "POST") {
+            ctx.body = "创建用户"
+        } else {
+            ctx.status = 405
+        }
+    } else if (ctx.url.match(/\/users\/(\w+)/) && ctx.method === "GET") {
+        let userId = ctx.url.match(/\/users\/(\w+)/)[1];
+        ctx.body = `用户id是 ${userId}`
+    } 
+    
+    else {
+        ctx.status = 404
+    }
 })
 
 app.listen(8000,() => {
