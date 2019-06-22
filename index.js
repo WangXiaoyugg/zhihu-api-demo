1ca1ee10aabc062fc88580fe805a56df1a91b115
GarenWang <2270099149@qq.com>
2019-06-22 14:36:09 +0800

    feature: 添加错误中间件

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const bodyParser = require("koa-bodyparser");
 const registerRouter = require("./app/routes");
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch(err) {
+    ctx.status = err.status || err.statusCode || 500;
+    ctx.body = {
+      message: err.message
+    }
+  }
+})
+
 app.use(bodyParser());
 registerRouter(app);
+
 
 app.listen(8000, () => {
   console.log("server is start at localhost:8000");
