a4d9d1ea334a931048ca7d91a74238b3c5f0bfe2
GarenWang <2270099149@qq.com>
2019-06-22 15:13:38 +0800

    feature: 使用koa-parameter增加参数校验

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -7,19 +7,30 @@ class UserController {
     findById(ctx) {
         if(ctx.params.id * 1 >= db.length) {
             ctx.throw(412)
-            return;
         }
         ctx.body = db[ctx.params.id * 1];
     }
     create(ctx) {
+        ctx.verifyParams({
+            name: {type: 'string', required: true}
+        })
         db.push(ctx.request.body);
         ctx.body = ctx.request.body;
     }
     update(ctx) {
+        if(ctx.params.id * 1 >= db.length) {
+            ctx.throw(412)
+        }
+        ctx.verifyParams({
+            name: {type: 'string', required: true}
+        })
         db[ctx.params.id * 1] = ctx.request.body;
         ctx.body = ctx.request.body;
     }
     deleteById(ctx) {
+        if(ctx.params.id * 1 >= db.length) {
+            ctx.throw(412)
+        }
         db.splice(ctx.params.id * 1, 1);
         ctx.status = 204;
     }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const error = require("koa-json-error");
+const parameter = require("koa-parameter")
 const registerRouter = require("./app/routes");
 const app = new Koa();
 
@@ -9,6 +10,7 @@ app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest},
 }));
 app.use(bodyParser());
+app.use(parameter(app));
 registerRouter(app);
 
 
