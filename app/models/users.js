24cee39d103a7b167d257566bef2c780bd2f8d62
GarenWang <2270099149@qq.com>
2019-06-23 10:06:49 +0800

    feature:实现用户注册

--- app/models/users.js
+++ app/models/users.js
@@ -1,7 +1,9 @@
 const { Schema, model} = require("mongoose");
 
 const userSchema = {
+    __v: {type: Number, select: false},
     name: {type: String, required: true},
+    password: {type: String, required: true, select: false},
 }
 
 module.exports = model("User", userSchema)
