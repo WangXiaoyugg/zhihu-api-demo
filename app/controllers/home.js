e7a3f85c6dbe0a49ee31b4b0a20642d940ff930f
GarenWang <2270099149@qq.com>
2019-06-23 14:56:35 +0800

    feature: koa-staic生成图片链接

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,13 @@
+const path = require("path");
+
 class HomeController {
     index(ctx) {
         ctx.body = "<h1>首页</h1>";
     }
     upload(ctx) {
         const file = ctx.request.files.file;
-        ctx.body = {path: file.path};
+        const basename = path.basename(file.path);
+        ctx.body = {url: `${ctx.origin}/uploads/${basename}`};
     }
 }
 
--- index.js
+++ index.js
@@ -2,32 +2,48 @@ const Koa = require("koa");
 const koaBody = require("koa-body");
 const error = require("koa-json-error");
 const parameter = require("koa-parameter");
-const mongoose = require("mongoose")
+const koaStatic = require("koa-static");
+const mongoose = require("mongoose");
 
 const path = require("path");
-const {mongo} = require('./config/db');
+const { mongo } = require("./config/db");
 const registerRouter = require("./app/routes");
 const app = new Koa();
 
-mongoose.connect(mongo.url, {useNewUrlParser: true, useFindAndModify: false}, () => {console.log("MongoDB connect success!")});
-mongoose.connection.on("error", console.error.bind(console, 'connection error:'));
-
-app.use(error({
-  postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest},
-}));
-app.use(koaBody({
-  multipart: true,
-  formidable: {
-    uploadDir: path.join(__dirname, "app/public/uploads"),
-    keepExtensions: true,
+mongoose.connect(
+  mongo.url,
+  { useNewUrlParser: true, useFindAndModify: false },
+  () => {
+    console.log("MongoDB connect success!");
   }
-}));
+);
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error:")
+);
+
+app.use(koaStatic(path.join(__dirname, "app/public")));
+
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === "production" ? rest : { stack, ...rest }
+  })
+);
+app.use(
+  koaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: path.join(__dirname, "app/public/uploads"),
+      keepExtensions: true
+    }
+  })
+);
 app.use(parameter(app));
 registerRouter(app);
-
 
 app.listen(8000, () => {
   console.log("server is start at localhost:8000");
 });
 
-// 
+//
