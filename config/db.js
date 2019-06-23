d1d3a89f29e7c05a10a060e231f3d91fff4e6910
GarenWang <2270099149@qq.com>
2019-06-23 08:23:26 +0800

    feature: 使用mongoose 连接云mongodb

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoPassword = '2012443944'
+
+module.exports = {
+    mongo: {
+        url: `mongodb+srv://garen:${mongoPassword}@cluster0-yognz.mongodb.net/test?retryWrites=true&w=majority`
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const error = require("koa-json-error");
-const parameter = require("koa-parameter")
+const parameter = require("koa-parameter");
+const mongoose = require("mongoose")
+
+const {mongo} = require('./config/db')
 const registerRouter = require("./app/routes");
 const app = new Koa();
 
+mongoose.connect(mongo.url, {useNewUrlParser: true}, () => {console.log("MongoDB connect success!")});
+mongoose.connection.on("error", console.error.bind(console, 'connection error:'));
 
 app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest},
@@ -17,3 +22,5 @@ registerRouter(app);
 app.listen(8000, () => {
   console.log("server is start at localhost:8000");
 });
+
+// 
