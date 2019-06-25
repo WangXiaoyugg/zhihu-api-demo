68b46aea7604d9644a70b75d026d60cc914a819c
GarenWang <2270099149@qq.com>
2019-06-26 06:20:13 +0800

    feature: 用户和话题的模糊搜索

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -13,7 +13,9 @@ class UserController {
         const {per_page = 10} = ctx.query
         const page = Math.max(ctx.query.page * 1, 1) - 1
         const perPage = Math.max(per_page * 1, 1)
-        ctx.body = await User.find().limit(perPage).skip(page * perPage)
+        ctx.body = await User
+            .find({name: new RegExp(ctx.query.q)})
+            .limit(perPage).skip(page * perPage)
     }
     async findById(ctx) {
         const { fields = ''} = ctx.query;
